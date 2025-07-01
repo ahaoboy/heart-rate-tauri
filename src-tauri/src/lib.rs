a47b8479a532f87104568d6101a59f682c9eb204
ahaoboy <504595380@qq.com>
2025-07-01 16:15:46 +0800

    add ui

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,6 @@
 use tauri::Emitter;
 
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
-#[tauri::command]
 fn start_monitoring(app: tauri::AppHandle) {
-    println!("start_monitoring");
     use tauri_plugin_shell::process::CommandEvent;
     use tauri_plugin_shell::ShellExt;
 
@@ -16,7 +8,6 @@ fn start_monitoring(app: tauri::AppHandle) {
     let (mut rx, mut _child) = sidecar_command.spawn().expect("Failed to spawn heart-rate");
 
     tauri::async_runtime::spawn(async move {
-        // read events such as stdout
         while let Some(event) = rx.recv().await {
             if let CommandEvent::Stdout(line_bytes) = event {
                 let line = String::from_utf8_lossy(&line_bytes);
@@ -26,44 +17,6 @@ fn start_monitoring(app: tauri::AppHandle) {
             }
         }
     });
-
-    // std::thread::spawn(move ||  {
-    //     use std::io::{BufRead, BufReader};
-    //     use std::process::{Command, Stdio};
-
-    //     let mut child = Command::new("c:/Users/ace/.ei/heart-rate")
-    //         .stdout(Stdio::piped())
-    //         .spawn()
-    //         .expect("Failed to start the executable");
-
-    //     let stdout = child.stdout.take().expect("Failed to capture stdout");
-    //     let reader = BufReader::new(stdout);
-
-    //     for line in reader.lines() {
-    //         match line {
-    //             Ok(line) => {
-    //                 println!("Received: {}", line);
-    //                 let _ = app_handle.emit("heart-rate", line);
-    //             }
-    //             Err(e) => eprintln!("Error reading line: {}", e),
-    //         }
-    //     }
-
-    //     let status = child.wait().expect("Failed to wait on child process");
-    //     println!("Child process exited with: {}", status);
-    // });
-
-    // std::thread::spawn(|| async move {
-    //     let Ok(monitor) = heart_rate::detect_monitor().await else {
-    //         println!("monitor not found");
-    //         return;
-    //     };
-
-    //     let mut receiver = monitor.start_monitoring().await;
-    //     while let Some(hr) = receiver.recv().await {
-    //         let _ = app_handle.emit("heart-rate", hr);
-    //     }
-    // });
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -75,7 +28,6 @@ pub fn run() {
             start_monitoring(app.handle().clone());
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![greet, start_monitoring])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,122 @@
-import { createSignal, onMount } from "solid-js";
+import { createEffect, createMemo, createSignal, onMount } from "solid-js";
 import "./App.css";
 import { listen } from "@tauri-apps/api/event";
+import { getAllWindows, PhysicalPosition, PhysicalSize } from "@tauri-apps/api/window";
+
+const COLORS = [
+  { range: 60, color: "#4682B4" },
+  { range: 70, color: "#87CEEB" },
+  { range: 80, color: "#90EE90" },
+  { range: 90, color: "#FFFF00" },
+  { range: 100, color: "#FFA500" },
+  { range: 110, color: "#FF6347" },
+  { range: 120, color: "#FF0000" },
+  { range: 255, color: "#8B0000" }
+];
+
+const DEFAULT_FONT_SIZE = 64;
+
+function measureTextSizeWithDOM(text: string, fontStyles: Record<string, string>) {
+  const span = document.createElement('span');
+
+  Object.assign(span.style, {
+    position: 'absolute',
+    visibility: 'hidden',
+    whiteSpace: 'nowrap',
+    ...fontStyles
+  });
+
+  span.textContent = text;
+
+  document.body.appendChild(span);
+
+  const size = {
+    width: span.offsetWidth,
+    height: span.offsetHeight
+  };
+  document.body.removeChild(span);
+  return size;
+}
+
+// ♥ (U+2665), ♡ (U+2661), and ❤ (U+2764)
+const HEART = "❤"
 
 function App() {
-  const [greetMsg, setGreetMsg] = createSignal("");
+  const [getRate, setRate] = createSignal(0)
+  const color = createMemo(() => {
+    const rate = getRate()
+    for (const i of COLORS) {
+      if (rate < i.range) {
+        return i.color
+      }
+    }
+    return COLORS[0].color
+  })
+
+  const text = createMemo(() => {
+    const rate = getRate()
+    return rate ? rate.toString() : "..."
+  }, "...")
 
   onMount(async () => {
     const inputHandle = await listen<number>("heart-rate", (event) => {
-      console.log('heart-rate', event)
+      setRate((+event.payload))
     })
-
-    console.log('inputHandle', inputHandle)
+    return inputHandle
   })
 
+  createEffect(async () => {
+    const s = [HEART, text()].join(' ')
+    const rect = measureTextSizeWithDOM(s, { fontSize: `${DEFAULT_FONT_SIZE}px` })
+    const v = await getAllWindows()
+    const win = v.find((win) => win.label === "main");
+    if (!win) {
+      return
+    }
+    win.setSize(new PhysicalSize(rect.width * 2, rect.height * 2))
+  })
+
+  let drag = false
+  let dragStartPosition = { x: 0, y: 0 }
+
   return (
-    <div></div>
+    <div id='main' style={{ color: color() }}
+      onMouseDown={e => {
+        // console.log('onMouseDown', e)
+        drag = true
+        dragStartPosition.x = e.offsetX
+        dragStartPosition.y = e.offsetY
+        console.log(dragStartPosition, drag, e)
+      }}
+
+      onMouseMove={async (e) => {
+        if (!drag) {
+          return
+        }
+        const v = await getAllWindows()
+        const win = v.find((win) => win.label === "main");
+        if (!win) {
+          return
+        }
+        const dx = e.offsetX - dragStartPosition.x
+        const dy = e.offsetY - dragStartPosition.y
+        const p = await win.outerPosition()
+        await win.setPosition(new PhysicalPosition(p.x + dx, p.y + dy))
+      }}
+      onmouseup={e => {
+        drag = false
+      }}
+    >
+      {/* TODO: suppor more styles */}
+      {/* <div id='heart'>
+        {HEART}
+      </div>
+      <div id="text">
+        {text()}
+      </div> */}
+
+      {HEART} {text()}
+    </div>
   );
 }
 
