e90ade98161e21dfce8a704714d2ec51bcbf6d3a
ahaoboy <504595380@qq.com>
2025-07-01 19:05:51 +0800

    add style

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import { createEffect, createMemo, createSignal, onMount } from "solid-js";
+import { Accessor, createEffect, createMemo, createSignal, onMount } from "solid-js";
 import "./App.css";
 import { listen } from "@tauri-apps/api/event";
 import { getAllWindows, PhysicalPosition, PhysicalSize } from "@tauri-apps/api/window";
 
 const COLORS = [
-  { range: 60, color: "#4682B4" },
-  { range: 70, color: "#87CEEB" },
-  { range: 80, color: "#90EE90" },
-  { range: 90, color: "#FFFF00" },
-  { range: 100, color: "#FFA500" },
-  { range: 110, color: "#FF6347" },
+  { range: 60, color: "#FFFF00" },
+  { range: 80, color: "#FFA500" },
+  { range: 100, color: "#FF6347" },
   { range: 120, color: "#FF0000" },
   { range: 255, color: "#8B0000" }
 ];
@@ -41,8 +38,54 @@ function measureTextSizeWithDOM(text: string, fontStyles: Record<string, string>
 // ♥ (U+2665), ♡ (U+2661), and ❤ (U+2764)
 const HEART = "❤"
 
+function Child({ style, n }: { style: number, n: Accessor<number> }) {
+  if (style === 0) {
+    return n()
+  }
+
+  if (style === 1) {
+    return [HEART, n].join("")
+  }
+
+  if (style === 2) {
+    const len = n.toString().length
+    let textSize = DEFAULT_FONT_SIZE
+    if (len <= 1) {
+      textSize = DEFAULT_FONT_SIZE
+    } else if (len <= 2) {
+      textSize = DEFAULT_FONT_SIZE / 2
+    } else if (len <= 3) {
+      textSize = (DEFAULT_FONT_SIZE / 3) || 0
+    }
+    return <><div id='heart'>{HEART}</div><div id="text" style={{ "font-size": `${textSize}px` }}>{n()}</div></>
+  }
+
+  return n()
+}
+
+function getSize(style: number, n: number) {
+  const len = n.toString().length;
+  if (style === 0) {
+    // only text
+    return { w: DEFAULT_FONT_SIZE * len, h: DEFAULT_FONT_SIZE * len }
+  }
+
+  // heart + text
+  if (style === 1) {
+    return { w: DEFAULT_FONT_SIZE * (len + 1), h: DEFAULT_FONT_SIZE * (len + 1) }
+  }
+
+  // text in heart
+  if (style === 1) {
+    return { w: DEFAULT_FONT_SIZE, h: DEFAULT_FONT_SIZE }
+  }
+
+  return { w: DEFAULT_FONT_SIZE, h: DEFAULT_FONT_SIZE }
+}
+
 function App() {
   const [getRate, setRate] = createSignal(0)
+  const [getStyle, setStyle] = createSignal(0)
   const color = createMemo(() => {
     const rate = getRate()
     for (const i of COLORS) {
@@ -53,10 +96,6 @@ function App() {
     return COLORS[0].color
   })
 
-  const text = createMemo(() => {
-    const rate = getRate()
-    return rate ? rate.toString() : "..."
-  }, "...")
 
   onMount(async () => {
     const inputHandle = await listen<number>("heart-rate", (event) => {
@@ -66,14 +105,13 @@ function App() {
   })
 
   createEffect(async () => {
-    const s = [HEART, text()].join(' ')
-    const rect = measureTextSizeWithDOM(s, { fontSize: `${DEFAULT_FONT_SIZE}px` })
+    const rect = getSize(getStyle(), getRate())
     const v = await getAllWindows()
     const win = v.find((win) => win.label === "main");
     if (!win) {
       return
     }
-    win.setSize(new PhysicalSize(rect.width * 2, rect.height * 2))
+    win.setSize(new PhysicalSize(rect.w, rect.h))
   })
 
   let drag = false
@@ -82,11 +120,9 @@ function App() {
   return (
     <div id='main' style={{ color: color() }}
       onMouseDown={e => {
-        // console.log('onMouseDown', e)
         drag = true
         dragStartPosition.x = e.offsetX
         dragStartPosition.y = e.offsetY
-        console.log(dragStartPosition, drag, e)
       }}
 
       onMouseMove={async (e) => {
@@ -103,20 +139,10 @@ function App() {
         const p = await win.outerPosition()
         await win.setPosition(new PhysicalPosition(p.x + dx, p.y + dy))
       }}
-      onmouseup={e => {
+      onmouseup={() => {
         drag = false
       }}
-    >
-      {/* TODO: suppor more styles */}
-      {/* <div id='heart'>
-        {HEART}
-      </div>
-      <div id="text">
-        {text()}
-      </div> */}
-
-      {HEART} {text()}
-    </div>
+    ><Child style={2} n={getRate} /></div>
   );
 }
 
